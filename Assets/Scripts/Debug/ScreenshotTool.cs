facd2ccee1b42d97309546d2c66999e9e372fdc5
Ryan McCloskey <rmccloskey8@gmail.com>
2015-04-22 17:17:04 +0100

    Added Screenshot Tool

---
+++ Assets/Scripts/Debug/ScreenshotTool.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenshotTool : MonoBehaviour {
+
+	void LateUpdate()
+	{
+		if(Input.GetKeyUp(KeyCode.F12))
+		{
+			Debug.Log( "Capturing Screenshot..." );
+			string fileName = "Screenshot_" + Random.Range( 0, 9999 ) + ".png";
+			Application.CaptureScreenshot( fileName, 4 );
+		}
+	}
+}
